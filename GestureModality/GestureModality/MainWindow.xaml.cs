718df5505af9a3c9f27725d93ede149818d7088e
Unknown <anafilipatavares@ua.pt>
2018-05-13 17:01:00 +0100

    Discrete gesture - tracking closest body (seated)

---
+++ GestureModality/GestureModality/MainWindow.xaml.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GestureModality
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Windows;
+    using System.Windows.Controls;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.VisualGestureBuilder;
+
+    /// <summary>
+    /// Interaction logic for the MainWindow
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        /// <summary> Active Kinect sensor </summary>
+        private KinectSensor kinectSensor = null;
+
+        /// <summary> Array for the bodies (Kinect will track up to 6 people simultaneously) </summary>
+        private Body[] bodies = null;
+
+        /// <summary> Reader for body frames </summary>
+        private BodyFrameReader bodyFrameReader = null;
+
+        /// <summary> Current status text to display </summary>
+        private string statusText = null;
+
+        /// <summary> KinectBodyView object which handles drawing the Kinect bodies to a View box in the UI </summary>
+        private KinectBodyView kinectBodyView = null;
+
+        /// <summary> List of gesture detectors, there will be one detector created for each potential body (max of 6) </summary>
+        private List<GestureDetector> gestureDetectorList = null;
+
+
+        private GestureDetector gestureDetector = null;
+
+
+        private Body currentTrackedBody = null;
+
+
+        private ulong currentTrackingId = 0;
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class
+        /// </summary>
+        public MainWindow()
+        {
+            // only one sensor is currently supported
+            this.kinectSensor = KinectSensor.GetDefault();
+
+            // set IsAvailableChanged event notifier
+            this.kinectSensor.IsAvailableChanged += this.Sensor_IsAvailableChanged;
+
+            // open the sensor
+            this.kinectSensor.Open();
+
+            // set the status text
+            this.StatusText = this.kinectSensor.IsAvailable ? Properties.Resources.RunningStatusText
+                                                            : Properties.Resources.NoSensorStatusText;
+
+            // open the reader for the body frames
+            this.bodyFrameReader = this.kinectSensor.BodyFrameSource.OpenReader();
+
+            // set the BodyFramedArrived event notifier
+            this.bodyFrameReader.FrameArrived += this.Reader_BodyFrameArrived;
+
+            // initialize the BodyViewer object for displaying tracked bodies in the UI
+            this.kinectBodyView = new KinectBodyView(this.kinectSensor);
+
+            // initialize the gesture detection objects for our gestures
+            this.gestureDetectorList = new List<GestureDetector>();
+
+            // initialize the MainWindow
+            this.InitializeComponent();
+
+            // set our data context objects for display in UI
+            this.DataContext = this;
+            this.kinectBodyViewbox.DataContext = this.kinectBodyView;
+
+            // create a gesture detector for each body (6 bodies => 6 detectors) and create content controls to display results in the UI
+            int col0Row = 0;
+            int col1Row = 0;
+            int maxBodies = 1;
+
+            GestureResultView result = new GestureResultView(0, false, false, 0.0f);
+            this.gestureDetector = new GestureDetector(this.kinectSensor, result);
+
+            // split gesture results across the first two columns of the content grid
+            ContentControl contentControl = new ContentControl();
+            contentControl.Content = this.gestureDetector.GestureResultView;
+
+            // Gesture results for bodies: 0, 2, 4
+            Grid.SetColumn(contentControl, 0);
+            Grid.SetRow(contentControl, col0Row);
+            this.contentGrid.Children.Add(contentControl);
+        }
+
+        /// <summary>
+        /// INotifyPropertyChangedPropertyChanged event to allow window controls to bind to changeable data
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Gets or sets the current status text to display
+        /// </summary>
+        public string StatusText
+        {
+            get
+            {
+                return this.statusText;
+            }
+
+            set
+            {
+                if (this.statusText != value)
+                {
+                    this.statusText = value;
+
+                    // notify any bound elements that the text has changed
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("StatusText"));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Execute shutdown tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (this.bodyFrameReader != null)
+            {
+                // BodyFrameReader is IDisposable
+                this.bodyFrameReader.FrameArrived -= this.Reader_BodyFrameArrived;
+                this.bodyFrameReader.Dispose();
+                this.bodyFrameReader = null;
+            }
+
+            if (this.gestureDetector != null)
+            {
+                this.gestureDetector.Dispose();
+
+            }
+
+            if (this.kinectSensor != null)
+            {
+                this.kinectSensor.IsAvailableChanged -= this.Sensor_IsAvailableChanged;
+                this.kinectSensor.Close();
+                this.kinectSensor = null;
+            }
+        }
+
+        /// <summary>
+        /// Handles the event when the sensor becomes unavailable (e.g. paused, closed, unplugged).
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Sensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs e)
+        {
+            // on failure, set the status text
+            this.StatusText = this.kinectSensor.IsAvailable ? Properties.Resources.RunningStatusText
+                                                            : Properties.Resources.SensorNotAvailableStatusText;
+        }
+
+
+        /// <summary>
+        /// Returns the length of a vector from origin
+        /// </summary>
+        /// <param name="point">Point in space to find it's distance from origin</param>
+        /// <returns>Distance from origin</returns>
+        private static double VectorLength(CameraSpacePoint point)
+        {
+            var result = Math.Pow(point.X, 2) + Math.Pow(point.Y, 2) + Math.Pow(point.Z, 2);
+
+            result = Math.Sqrt(result);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Finds the closest body from the sensor if any
+        /// </summary>
+        /// <param name="bodyFrame">A body frame</param>
+        /// <returns>Closest body, null of none</returns>
+        private static Body FindClosestBody(BodyFrame bodyFrame)
+        {
+            Body result = null;
+            double closestBodyDistance = double.MaxValue;
+
+            Body[] bodies = new Body[bodyFrame.BodyCount];
+            bodyFrame.GetAndRefreshBodyData(bodies);
+
+            foreach (var body in bodies)
+            {
+                if (body.IsTracked)
+                {
+                    var currentLocation = body.Joints[JointType.SpineBase].Position;
+
+                    var currentDistance = VectorLength(currentLocation);
+
+                    if (result == null || currentDistance < closestBodyDistance)
+                    {
+                        result = body;
+                        closestBodyDistance = currentDistance;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Find if there is a body tracked with the given trackingId
+        /// </summary>
+        /// <param name="bodyFrame">A body frame</param>
+        /// <param name="trackingId">The tracking Id</param>
+        /// <returns>The body object, null of none</returns>
+        private static Body FindBodyWithTrackingId(BodyFrame bodyFrame, ulong trackingId)
+        {
+            Body result = null;
+
+            Body[] bodies = new Body[bodyFrame.BodyCount];
+            bodyFrame.GetAndRefreshBodyData(bodies);
+
+            foreach (var body in bodies)
+            {
+                if (body.IsTracked)
+                {
+                    if (body.TrackingId == trackingId)
+                    {
+                        result = body;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Handles the body frame data arriving from the sensor and updates the associated gesture detector object for each body
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Reader_BodyFrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                if (bodyFrame != null)
+                {
+                    if (this.currentTrackedBody != null)
+                    {
+                        this.currentTrackedBody = FindBodyWithTrackingId(bodyFrame, this.currentTrackingId);
+
+                        if (this.currentTrackedBody != null)
+                        {
+                            this.kinectBodyView.UpdateBodyFrame(currentTrackedBody);
+                            return;
+                        }
+                    }
+
+                    Body selectedBody = FindClosestBody(bodyFrame);
+
+                    if (selectedBody == null)
+                    {
+                        return;
+                    }
+
+                    this.currentTrackedBody = selectedBody;
+                    this.currentTrackingId = selectedBody.TrackingId;
+                    this.gestureDetector.TrackingId = currentTrackingId;
+                    Console.WriteLine(currentTrackingId);
+
+                    this.kinectBodyView.UpdateBodyFrame(currentTrackedBody);
+
+                    // if the current body is tracked, unpause its detector to get VisualGestureBuilderFrameArrived events
+                    // if the current body is not tracked, pause its detector so we don't waste resources trying to get invalid gesture results
+                    this.gestureDetector.IsPaused = currentTrackingId == 0;
+
+                }
+            }
+        }
+    }
+}
